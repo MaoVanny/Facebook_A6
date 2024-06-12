9d15297bb8facb6f36177b1eb015f3a877b1f7f5
Your Name <you@example.com>
2024-06-12 10:07:15 +0700

    [#4] Update on crud comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -45,4 +45,7 @@ class Comment extends Model
     public function user():BelongsTo{
         return $this->belongsTo(User::class);
     }
+    public function post():BelongsTo{
+        return $this->belongsTo(Post::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -58,6 +58,7 @@ class User extends Authenticatable
     {
         return $this->hasMany(Comment::class);
     }
+    
 
     public function likes()
     {
