06a76d2307abc44ac7171d29220e228c3910fd0e
unknown <vanny.mao@student.passerellesnumeriques.org>
2024-06-12 09:18:04 +0700

    fix code

--- database/migrations/2024_06_10_044551_create_likes_table.php
+++ database/migrations/2024_06_10_044551_create_likes_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('post_id');
-            $table->boolean('liked')->default(true);
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
